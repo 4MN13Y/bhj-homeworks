5c54e0bbba9d07ad48fadcd356193b0716101caf
Yura <psnfifka@gmail.com>
2023-03-13 15:46:19 +0200

    done

---
+++ dom/reveal/task.js
@@ -0,0 +1,34 @@
+const reveal = document.querySelectorAll(".reveal");
+
+function isVisible(el) {
+  const { bottom, top } = el.getBoundingClientRect();
+  if (bottom < 0 || top > window.innerHeight) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function addClass(el) {
+  if (isVisible(el)) {
+    el.classList.add("reveal_active");
+  } else {
+    el.classList.remove("reveal_active");
+  }
+}
+
+reveal.forEach((el) => {
+  window.addEventListener("scroll", () => addClass(el));
+});
+
+//======= ========= ========== =========================
+
+// reveal.forEach((el) => {
+//   window.addEventListener("scroll", () => {
+//     if (isVisible(el)) {
+//       el.classList.add("reveal_active");
+//     } else {
+//       el.classList.remove("reveal_active");
+//     }
+//   });
+// });
